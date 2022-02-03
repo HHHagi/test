7d1675005af57b86ead26ec2283eac7df39f6dc3
HHHagi <hahaha3500@gmail.com>
2022-02-03 10:21:43 +0900

    掲示板の作成（DBへの投稿の保存と取得）

---
+++ send.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<title>掲示板</title>
+<h1>掲示板</h1>
+<section>
+    <h2>投稿が完了しました。</h2>
+    <button onclick="location.href='test3_1.php'">投稿一覧へ戻る</button>
+</section>
+
+<?php
+$id = null;
+$name = $_POST["name"];
+$contents = $_POST["contents"];
+date_default_timezone_set('Asia/Tokyo');
+$created_at = date("Y-m-d H:i:s");
+//DB接続情報を設定。
+$pdo = new PDO(
+    "mysql:dbname=work;host=localhost",
+    "root",
+    "root",
+    array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET `utf8`")
+);
+//SQLを実行。
+$regist = $pdo->prepare("INSERT INTO post(id, name, contents, created_at) VALUES (:id,:name,:contents,:created_at)");
+$regist->bindParam(":id", $id);
+$regist->bindParam(":name", $name);
+$regist->bindParam(":contents", $contents);
+$regist->bindParam(":created_at", $created_at);
+$regist->execute();
+?>

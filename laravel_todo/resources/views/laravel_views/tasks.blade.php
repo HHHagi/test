fba412185715408eb0b428c220e4035e57811f92
HHHagi <hahaha3500@gmail.com>
2022-03-20 20:47:17 +0900

    JSの変数をletからconstへ修正。

--- laravel_todo/resources/views/laravel_views/tasks.blade.php
+++ laravel_todo/resources/views/laravel_views/tasks.blade.php
@@ -59,30 +59,30 @@
 
 <script>
      function valueChange(event) {
-          let task_all = document.getElementsByClassName("all");
+          const task_all = document.getElementsByClassName("all");
                for (var i = 0; i < task_all.length; i++) {
                     task_all[i].style.display = "table-row";
                }
           if (event.currentTarget.value === "work") {
-               let task_done = document.getElementsByClassName("done");
+               const task_done = document.getElementsByClassName("done");
                for (var i = 0; i < task_done.length; i++) {
                     task_done[i].style.display = "none";
                }
           } else if (event.currentTarget.value === "done") {
-               let task_work = document.getElementsByClassName("work"); 
+               const task_work = document.getElementsByClassName("work"); 
                for (var i = 0; i < task_work.length; i++) {
                     task_work[i].style.display = "none";
                } 
           }
      }
-     let all = document.getElementById('all');
+     const all = document.getElementById('all');
      all.checked = true;
      all.addEventListener('change', valueChange);
 
-     let work = document.getElementById('work');
+     const work = document.getElementById('work');
      work.addEventListener('change', valueChange);
 
-     let done = document.getElementById('done');
+     const done = document.getElementById('done');
      done.addEventListener('change', valueChange);
 </script>
 

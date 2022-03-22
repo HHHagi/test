bd73b54ee8d8ebc8c2667bd9656340f926f7688a
HHHagi <hahaha3500@gmail.com>
2022-03-22 12:16:06 +0900

    JSを別ファイルへ切り出し。JSの記述の修正。

--- laravel_todo/resources/views/laravel_views/tasks.blade.php
+++ laravel_todo/resources/views/laravel_views/tasks.blade.php
@@ -57,33 +57,6 @@
      <button type="submit">追加</button>
 </form>
 
-<script>
-     function valueChange(event) {
-          const task_all = document.getElementsByClassName("all");
-               for (var i = 0; i < task_all.length; i++) {
-                    task_all[i].style.display = "table-row";
-               }
-          if (event.currentTarget.value === "work") {
-               const task_done = document.getElementsByClassName("done");
-               for (var i = 0; i < task_done.length; i++) {
-                    task_done[i].style.display = "none";
-               }
-          } else if (event.currentTarget.value === "done") {
-               const task_work = document.getElementsByClassName("work"); 
-               for (var i = 0; i < task_work.length; i++) {
-                    task_work[i].style.display = "none";
-               } 
-          }
-     }
-     const all = document.getElementById('all');
-     all.checked = true;
-     all.addEventListener('change', valueChange);
-
-     const work = document.getElementById('work');
-     work.addEventListener('change', valueChange);
-
-     const done = document.getElementById('done');
-     done.addEventListener('change', valueChange);
-</script>
+<script src="{{ asset('/js/task_status.js')}}"></script>
 
 @endsection
